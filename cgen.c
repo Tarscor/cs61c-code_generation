0f9785aca494213ce1fc9c1c88e264f42a06da29
Tarsus Lam <tarususu.t3@berkeley.edu>
2019-03-01 05:08:14 -0800

    next

--- cgen.c
+++ cgen.c
@@ -586,7 +586,7 @@ void processIfElse(DAST* dast, char* startLabel, char* endLabel) {
     char* else_case = generateLocalLabel();
     char* end = generateLocalLabel();
     
-    dispatch(dast->children[0], startLabel, endLabel));
+    dispatch(dast->children[0], startLabel, endLabel);
     emitBNEZ(S1, true_case);
     
     if (dast->size < 3) {
